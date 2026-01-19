c0db7bdfebd5d84a672c640d13bd4926bcce6add
Beatriz Galarza <beatriz.glrz@gmail.com>
2026-01-18 23:21:30 -0800

    Add multiple utility functions

--- journal.js
+++ journal.js
@@ -2,3 +2,27 @@
 function greet(name) {
   return "Hello, " + name + "!";
 }
+
+// Greeting based on time of day
+function timeGreet(name, hour) {
+  if (hour < 12) {
+    return "Good morning, " + name + "!";
+  } else {
+    return "Good evening, " + name + "!";
+  }
+}
+
+// Add two numbers
+function add(a, b) {
+  return a + b;
+}
+
+// Subtract two numbers
+function subtract(a, b) {
+  return a - b;
+}
+
+// Check if number is positive
+function isPositive(num) {
+  return num > 0;
+}
